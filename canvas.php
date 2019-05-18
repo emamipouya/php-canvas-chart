7b35866a2909d29193c2cbfb55eac978678c440a
pouya <45901393+emamipouya@users.noreply.github.com>
2019-05-18 20:25:55 +0430

    Update canvas.php

--- canvas.php
+++ canvas.php
@@ -7,8 +7,6 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
  
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
  
 /**
  * canvas Model
