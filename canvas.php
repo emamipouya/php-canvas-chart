9d8d1b8591f60f2a5772ba3074d47eeb5b814c87
pouya <45901393+emamipouya@users.noreply.github.com>
2019-06-13 16:54:05 +0430

    Update canvas.php

--- canvas.php
+++ canvas.php
@@ -64,8 +64,118 @@ foreach($data as $var){
      $res.=$this->endScript();
   return $res;
   }
-  ///////////////////////////////////////////
+  /*
+*   Draw Gauge Graph 
+*/
+  function graphGauge($id,$width,$height,$font,$n,$r,$lowestBest,$title){
+      $border=0;
+        $res=$this->create($id,$width,$height,$border);
+        $res.=$this->getCanvas($id);
+    $res.=$this->setFont($id,$font);
+    $res.=$this->initiateGauge($id,$r,$n,$lowestBest,$title,$font);
+    $res.=$this->endScript();
+     return $res;
+    
+  }
+
+/**
+ * Initialize Gauge
+ * 
+ */
   
+  function initiateGauge($id,$r,$n,$lowestBest,$title,$font){
+      $func=$id.'_action';
+        $scr="var teta=0.0;
+            var stop=false;
+            var n=$n;
+            var count=0;
+            var xc=170;
+            var yc=200;
+            var r=$r;
+            var r1=r+20;
+            var r2=r+40;
+            var r3=r+27;
+            var lowbeter=$lowestBest;";
+            
+    $scr="document.addEventListener('DOMContentLoaded', $func);
+    function $func(){
+    $scr;
+    $id.fillStyle = '#000';";
+     $scr.="   $id.beginPath();";
+     $scr.="   $id.arc(xc,yc,r2+30,0.1,Math.PI-0.1,true);";
+    $scr.="    $id.closePath();";
+    $scr.="    $id.fill();
+        
+            $id.fillStyle = '#AAF';
+        a=0.0;
+        for(i=0;i<=10;i++)
+        {
+            j=i*10;
+            x=xc-r2*Math.cos(a);
+            y=yc-r2*Math.sin(a);
+            $id.fillText(j, x+10, y); 
+            a+=0.308;
+        }
+         $id.fillStyle = '#333';
+        $id.fillText('$title', xc+r2, yc+40); 
+        $id.fillStyle = '#eee';
+        $id.beginPath();
+        $id.arc(xc,yc,r1+1,0.1,Math.PI-0.1,true);
+        $id.closePath();
+        $id.fill();
+
+
+        //var grd=$id.createRadialGradient(xc-r1,yc,r1,xc+r1,yc,r1);
+        var grd=$id.createLinearGradient(xc-r1,yc,xc+r1,yc);
+        if(lowbeter===1){
+        grd.addColorStop(1,'red');
+        grd.addColorStop(.5, 'gray');
+        grd.addColorStop(0,'green');
+        }
+        else
+        {
+        grd.addColorStop(0,'red');
+        grd.addColorStop(.5, 'gray');
+        grd.addColorStop(1,'green'); 
+        }
+        
+        $id.fillStyle =grd;// 'hsl(220,60%,85%)';
+        //$id.fillStyle = '#EEF';
+        $id.beginPath();
+        $id.arc(xc,yc,r1,0.1,Math.PI-0.1,true);
+        $id.closePath();
+        $id.fill();
+        
+        $id.fillStyle = '#AAF';
+        $id.beginPath();
+        $id.arc(xc,yc,15,0,Math.PI,true);
+        $id.closePath();
+        $id.fill();
+        
+        $id.strokeStyle = 'hsl( 30,99%,50%)'; 
+                teta=0.0308*n;
+        x1=xc-r3*Math.cos(teta);
+        y1=yc-r3*Math.sin(teta);
+        
+        $id.beginPath();
+        $id.lineWidth=8;
+        
+        $id.moveTo(xc,yc);
+        $id.lineTo(x1,y1);
+        $id.stroke();
+       // teta+=0.0308;
+
+
+    if(count===n)
+        stop=true;
+
+    ";
+
+return $scr;
+  }
+ /**
+  *setBackground
+ */
   function setBackground($id,$dataURL ){
 
 $scr="var backgroundImage = new Image(); ";
