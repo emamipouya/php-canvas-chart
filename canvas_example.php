9a262801917cbd78568902b3d70ea3dcef9083b8
pouya <45901393+emamipouya@users.noreply.github.com>
2019-06-13 16:57:32 +0430

    Update canvas_example.php
    New Update included Gauge drawing in Canvas

--- canvas_example.php
+++ canvas_example.php
@@ -9,7 +9,7 @@ include 'canvas.php';
         $data['6']=55;
         
     $_canvas = new canvasMaker();
-      $id='eqCanvas2'; $width='550'; $height='380';
+      $id='bar'; $width='550'; $height='380';
      $bgURL = 'http://brainkava.ir/images/eq1.png';
      $font='17px Arial';
      
@@ -22,6 +22,13 @@ $xgap=70;
   $wd=27;
   
    $scr =$_canvas->graphColumn($id,$width,$height,$data,$bgURL,$font,$startPoint2,$max1,$gap,$xgap,$wd,$scale);
+
+   $val=$data['5'];
+   $id2='gauge';
+   $r=100;
+    $lowestBest=1;
+    $title="Test Gauge in Canvas";
+   $scr .=  $this->canvas->graphGauge($id2,$width,$height,$font,$val,$r,$lowestBest,$title);
 
 echo "<html><body> $scr </body></html>";
 ?>
